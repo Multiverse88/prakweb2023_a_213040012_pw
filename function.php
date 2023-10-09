34067ca98d3796b7036183016627b88d2fe99229
Multiverse88 <ainanihj@gmail.com>
2023-10-09 19:45:38 +0700

    tugas pw pertemuan 3

---
+++ function.php
@@ -0,0 +1,18 @@
+<?php
+$conn = mysqli_connect("localhost","root","", "prakweb_2023_a_213040012");
+
+
+function query ($query) {
+    global $conn;
+    $result = mysqli_query($conn,$query);
+    $rows = [];
+    while($row = mysqli_fetch_assoc($result)){
+        $rows[] = $row;
+    }
+    return $rows;
+
+}
+
+
+
+?>
